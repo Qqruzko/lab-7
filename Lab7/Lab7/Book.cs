f54a15acc4517d5547eea455d7b329d27d1b4758
Qqruzko <150928737+Qqruzko@users.noreply.github.com>
2023-11-27 23:15:29 +0300

    Add files via upload

---
+++ Lab7/Lab7/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Book : IComparable
+    {
+         string author;
+         string title;
+         int pages;
+         int year;
+       
+        public string Format(object value)
+        {
+            
+            return year.ToString();
+        }
+        int IComparable.CompareTo(object obj)
+        {
+            Book it = (Book)obj;
+            if (this.year == it.year) return 0;
+            else if (this.year > it.year) return 1;
+            else return -1;
+        }
+        public void SetBook(String author, String title, int pages,
+int year)
+        {
+            this.author = author;
+            this.title = title;
+            this.pages = pages;
+            this.year = year;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Книга: Автор: {0} Название: {1}Год издания:{3}  {2}стр.", author, title, pages,year);
+ }
+    }
+   
+    }
---
+++ Lab7/Lab7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Book book1 = new Book();
+            Book book2 = new Book();
+            Book book3 = new Book();
+            Book book4 = new Book();
+            Book book5 = new Book();
+            object[] books = new object[] { book1, book2,book3,book4,book5};
+            
+            
+            book1.SetBook("aboba","amobus",30,1980);
+            book2.SetBook("aboba", "amobus", 30, 1480);
+            book3.SetBook("aboba", "amobus", 30, 1983);
+            book4.SetBook("aboba", "amobus", 30, 1946);
+            book5.SetBook("aboba", "amobus", 30, 1540);
+            book1.Show();
+            Array.Sort(books);
+            string ab = book1.Format(book1);
+            Console.WriteLine(ab);
+
+            foreach(object b in books)
+            {
+                Book ini = (Book)b;
+                string a = ini.Format(ini);
+                Console.WriteLine("Book year ={0}",a);
+            }
+            Console.Read();
+        }
+        
+           
+        
+    }
+}
